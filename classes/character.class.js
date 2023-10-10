c5839f595cbbf86323e07513063d90245d06ada8
Mario-Mohar <iam@themo.at>
2023-10-10 10:37:09 +0200

    first animate Pepe

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,17 +3,31 @@
     height = 350;
     width = 350;
     y = 240;
-
-    constructor() {
-        super().loadImg('./img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
+    Images_Walking = [
             './img/2_character_pepe/2_walk/W-21.png',
             './img/2_character_pepe/2_walk/W-22.png',
             './img/2_character_pepe/2_walk/W-23.png',
             './img/2_character_pepe/2_walk/W-24.png',
             './img/2_character_pepe/2_walk/W-25.png',
             './img/2_character_pepe/2_walk/W-26.png',
-        ])
+    ];
+    currentImage = 0;
+
+    constructor() {
+        super().loadImg('./img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.Images_Walking);
+
+        this.animate();
+
+    }
+
+    animate(){
+
+        setInterval( () => {
+        let path = this.Images_Walking[this.currentImage];
+        this.img = this.imgCache[path];
+        this.currentImage++;
+    }, 1000);
     }
 
     jump() {
